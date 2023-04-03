31066936bc1b450e98aacc31da9b26ed58d857d8
Mahdi <mahdi.yousefi.skola.taby.se>
2023-04-03 14:32:17 +0200

    första commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.focus;
+const SCREENWIDTH = innerWidth;
+const SCREENHEIGHT = innerHeight;
+let gameCanvas = document.getElementById("gameCanvas");
+let c = gameCanvas.getContext("2d");
+gameCanvas.height = SCREENHEIGHT / 2;
+gameCanvas.width = SCREENWIDTH / 2;
+
+
